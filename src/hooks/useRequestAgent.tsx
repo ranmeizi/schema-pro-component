337c5dded5cf9f49dde97ff7553492ccc3e51aac
boboan <360969885@qq.com>
2024-06-17 10:26:43 +0800

    fix: 外部处理action错误

--- src/hooks/useRequestAgent.tsx
+++ src/hooks/useRequestAgent.tsx
@@ -132,8 +132,6 @@ export function useTableAction(
 
             return request(actions.create, 'POST', params).then((res) => {
               message.success('创建成功');
-            }).catch(()=>{
-              message.success('创建失败')
             })
           },
     [request, overrideActions.create],
@@ -151,8 +149,6 @@ export function useTableAction(
 
             return request(actions.updateById, 'POST', params).then((res) => {
               message.success('更新成功');
-            }).catch(()=>{
-              message.success('更新失败')
             })
           },
     [request, overrideActions.updateById],
@@ -170,8 +166,6 @@ export function useTableAction(
 
             return request(actions.deleteById, 'POST', params).then((res) => {
               message.success('删除成功');
-            }).catch(()=>{
-              message.success('删除失败')
             })
           },
     [request, overrideActions.deleteById],
